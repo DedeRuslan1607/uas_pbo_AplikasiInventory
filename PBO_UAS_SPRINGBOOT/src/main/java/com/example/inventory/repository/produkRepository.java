d9af9edb697ba84b8d3cd6ad3a16111a0915343f
DedeRuslan1607 <167685339+DedeRuslan1607@users.noreply.github.com>
2024-06-28 16:44:43 +0700

    Delete PBO_UAS_SPRINGBOOT directory

--- PBO_UAS_SPRINGBOOT/src/main/java/com/example/inventory/repository/produkRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.inventory.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.inventory.model.Produk;
-@Repository
-public interface produkRepository  extends JpaRepository<Produk, Long>{
-
-}
